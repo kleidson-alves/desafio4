662c88495237b475ea315dea6c459c73bd48f5d8
kleidson-alves <kleidsonalves15@gmail.com>
2021-01-11 17:22:14 -0300

    desafio

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,6 +1,12 @@
 import TransactionsRepository from '../repositories/TransactionsRepository';
 import Transaction from '../models/Transaction';
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class CreateTransactionService {
   private transactionsRepository: TransactionsRepository;
 
@@ -8,8 +14,17 @@ class CreateTransactionService {
     this.transactionsRepository = transactionsRepository;
   }
 
-  public execute(): Transaction {
-    // TODO
+  public execute({ title, value, type }: CreateTransaction): Transaction {
+    const balance = this.transactionsRepository.getBalance();
+    if (type === 'outcome' && value > balance.total) {
+      throw Error('Saldo insuficiente');
+    }
+    const transaction = this.transactionsRepository.create({
+      title,
+      value,
+      type,
+    });
+    return transaction;
   }
 }
 
